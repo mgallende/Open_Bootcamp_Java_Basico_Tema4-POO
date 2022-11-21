084632c7960db59a72f29db6fa1ddeebca37a1a2
Manuel Angel Gómez Allende <81536814+mgallende@users.noreply.github.com>
2022-11-21 23:07:16 +0100

    Update SmartDevice.java

--- src/poo/MainDevices/SmartDevice.java
+++ src/poo/MainDevices/SmartDevice.java
@@ -1,32 +1,18 @@
-package poo.clases;
+package poo.MainDevices;
 
 public class SmartDevice {
 
     //Atributos de la superclase
-    protected String descripcion;
-    protected String fabricante;
-    protected String modelo;
-    protected String Procesador;
-    protected String dimensiones;
-    protected String bateria;
-    protected int peso;
-    protected String conectividad;
-
-
-    // Constructores
+    String descripcion;
+    String fabricante;
+    String modelo;
 
     public SmartDevice(){
-
     }
 
-    public SmartDevice(String descripcion, String fabricante, String modelo, String procesador, String dimensiones, String bateria, int peso, String conectividad) {
+    public SmartDevice(String descripcion, String fabricante, String modelo) {
         this.descripcion = descripcion;
         this.fabricante = fabricante;
         this.modelo = modelo;
-        Procesador = procesador;
-        this.dimensiones = dimensiones;
-        this.bateria = bateria;
-        this.peso = peso;
-        this.conectividad = conectividad;
     }
 }
